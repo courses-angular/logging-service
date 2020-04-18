0ac8f466fcc19dc23d37c407deff5fdd3b12c162
yevgenyl <yevgeny@consist.co.il>
2020-04-18 14:22:43 +0300

    add LogConsole class

--- src/app/models/log-publisher.ts
+++ src/app/models/log-publisher.ts
@@ -1,9 +1,24 @@
 import {LogEntry} from '../shared/log.service';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 
-export abstract class ILogPublisher {
+export abstract class LogPublisher {
   location: string;
 
   abstract log(record: LogEntry): Observable<boolean>;
   abstract clear(): Observable<boolean>;
+}
+
+export class LogConsole extends LogPublisher {
+
+  log(record: LogEntry): Observable<boolean> {
+    console.log(record.buildLogString());
+
+    return of(true);
+  }
+
+  clear(): Observable<boolean> {
+    console.clear();
+
+    return of(true);
+  }
 }
