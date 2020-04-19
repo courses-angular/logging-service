5d8450abe8998597a8db044c0ded689c23f1e087
yevgenyl <yevgeny@consist.co.il>
2020-04-19 11:44:54 +0300

    adding publishers.json feat(log-publishers)modify build publishers

--- src/app/shared/log-publishers.service.ts
+++ src/app/shared/log-publishers.service.ts
@@ -12,17 +12,40 @@ const PUBLISHERS_FILE = 'assets/log-publishers.json';
 export class LogPublishersService {
   publishers: LogPublisher[] = [];
 
+
   constructor(private http: HttpClient) {
     this.buildPublishers();
+
   }
 
   buildPublishers() {
-    // Create an instance of the LogConsole Class
-    this.publishers.push(new LogConsole());
-    // Create an instance of the LogLocalStorage Class
-    this.publishers.push(new LogLocalStorage());
-    // Create an instance of the LogWebAPI Class
-    this.publishers.push(new LogWebAPI(this.http));
+    let publisher: LogPublisher;
+    // // Create an instance of the LogConsole Class
+    // this.publishers.push(new LogConsole());
+    // // Create an instance of the LogLocalStorage Class
+    // this.publishers.push(new LogLocalStorage());
+    // // Create an instance of the LogWebAPI Class
+    // this.publishers.push(new LogWebAPI(this.http));
+
+    this.getLoggers().subscribe(response => {
+      for (const pub of response.filter(p => p.isActive)) {
+        switch (pub.loggerName.toLowerCase()) {
+          case 'console':
+            publisher = new LogConsole();
+            break;
+          case 'localStorage':
+            publisher = new LogLocalStorage();
+            break;
+          case 'webapi':
+            publisher = new LogWebAPI(this.http);
+            break;
+        }
+        // Set location,if any of the logging
+        publisher.location = pub.loggerLocation;
+        //  Add publisher to Publishers Array
+        this.publishers.push(publisher);
+      }
+    });
   }
 
   getLoggers(): Observable<LogPublisherConfig[]> {
